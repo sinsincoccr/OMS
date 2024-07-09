16806d72b30e3b5cb601d98520b926e646209257
hk <sinsincoccr@naver.com>
2024-07-09 22:10:26 +0900

    [로그인] 비밀번호 보안 sha256 추가

--- order/src/main/java/projectOMS/order/controller/MemberController.java
+++ order/src/main/java/projectOMS/order/controller/MemberController.java
@@ -11,6 +11,9 @@ import projectOMS.order.domain.ItemCategoryVO;
 import projectOMS.order.domain.MemberVO;
 import projectOMS.order.service.MemberService;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -30,8 +33,25 @@ public class MemberController {
 
     @PostMapping("/memberSignIn")
     public String memberSignInPro(MemberVO member) {
+        String hashedPassword = hashPassword(member.getCpn_pw());
+        member.setCpn_pw(hashedPassword);
         memberService.memberSignIn(member);
         return "redirect:/"; // 회원 상세 페이지로 이동
+    }
+    private String hashPassword(String password) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] encodedHash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : encodedHash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     // 아이디 중복체크
